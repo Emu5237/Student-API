9adfe34cee893feed8647b9a93a124395f3460b4
tasli <taslimaemi038@gmail.com>
2022-05-28 12:41:55 +0600

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Students.Data;
+using Students.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudent db;
+        public  StudentController(IStudent _db)
+        {
+            db = _db;
+        }
+        [HttpPost]
+        public IActionResult Save([FromBody] Student data)
+        {
+            if (data == null)
+            {
+                return BadRequest();
+            }
+            db.Save(data);
+            return Ok(data);
+        }
+        [HttpGet("{Id}")]
+        public IActionResult GetStudent(int? Id)
+        {
+            Student data = db.GetStudent(Id);
+            return Ok(data);
+        }
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            IQueryable<Student> data = db.GetStudents;
+            return Ok(data);
+        }
+        [HttpDelete]
+        public IActionResult Delete(int? Id)
+        {
+            db.Delete(Id);
+            return Ok();
+        }
+    }
+}
